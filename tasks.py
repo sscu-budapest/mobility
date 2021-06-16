87cad48415166e0a9e4844800b4c064e0edc2a1e
Endre Márk Borza <endremborza@gmail.com>
2021-06-17 00:22:44 +0200

    rm dvc init task

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from invoke import task
 
-from src.data_locs import parts_root, dvc_cache_root
+from src.data_locs import  dvc_cache_root
 from src import pipereg
 
 
@@ -11,10 +11,3 @@ for pe in pipereg.steps:
 @task
 def lint(c):
     c.run("black src tasks.py; flake8 src --ignore=W503,E501; isort src -m 3 --tc")
-
-
-@task
-def dvc_external_init(c):
-    dvc_cache_root.mkdir(exist_ok=True)
-    c.run(f"dvc cache dir {dvc_cache_root}")
-    c.run(f"dvc add --external {parts_root} --no-commit")
