ebff9390f8dc3ef34f7346b914f77d052635b5c0
Endre Márk Borza <endremborza@gmail.com>
2021-07-23 14:29:26 +0200

    extend client get

--- src/util.py
+++ src/util.py
@@ -1,10 +1,10 @@
 from dask.distributed import Client, LocalCluster, get_client
 
 
-def get_dask_client(n_workers=3):
+def get_dask_client(n_workers=3, **kwargs):
     #  just until we can get some better equipment
     try:
         return get_client()
     except ValueError:
-        cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+        cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1, **kwargs)
         return Client(cluster)
