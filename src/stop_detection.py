e8e03c4a4e1ba52ec8d71390c067ebd3fa68a626
Endre Márk Borza <endremborza@gmail.com>
2022-03-07 11:51:39 +0100

    add speed to stop detection

--- src/stop_detection.py
+++ src/stop_detection.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from functools import partial
 
+import geopandas
 import pandas as pd
 from atqo import parallel_map
 from colassigner import ColAssigner
@@ -34,6 +35,12 @@ class Duration(CompositeTypeBase):
     end = datetime
 
 
+class Arrival(CompositeTypeBase):
+    speed = float  # kph
+    distance = float  # mins
+    time = float  # meters
+
+
 class StopFeatures(TableFeaturesBase):
     device_id = str
     year_month = str
@@ -45,6 +52,8 @@ class StopFeatures(TableFeaturesBase):
     center = um.Coordinates
     is_home = bool
     is_work = bool
+    from_last_stop = Arrival
+    from_last_ping = Arrival
 
 
 class Labeler(ColAssigner):
@@ -93,7 +102,51 @@ class Labeler(ColAssigner):
         return (_col == e1) & (_col == el)
 
 
-base_groupers = [FilteredPingFeatures.device_id, FilteredPingFeatures.year_month, FilteredPingFeatures.dayofmonth]
+class LocalCoords(ColAssigner):
+    def __init__(self, df) -> None:
+        self.garr = geopandas.points_from_xy(
+            df[StopFeatures.center.lon], df[StopFeatures.center.lat], crs="EPSG:4326"
+        ).to_crs("EPSG:23700")
+
+    def local_x(self, _):
+        return self.garr.x
+
+    def local_y(self, _):
+        return self.garr.y
+
+def pipe_assigner(df, assigner):
+    return assigner(df)(df)
+
+
+def add_arrivals(df, cols: Arrival):
+    tcol, dcol, scol = cols.time, cols.distance, cols.speed
+    return df.assign(
+        **{
+            tcol: (df[StopFeatures.interval.start] - df[StopFeatures.interval.end].shift(1)).dt.total_seconds() / 60,
+            dcol: df[[LocalCoords.local_x, LocalCoords.local_y]].pipe(
+                lambda _df: ((_df - _df.shift(1)) ** 2).sum(axis=1) ** 0.5
+            ),
+            scol: lambda _df: (_df[dcol] / 1000) / (_df[tcol] / 60),
+        }
+    )
+
+
+def add_speed_cols(df: pd.DataFrame):
+    return df.groupby(StopFeatures.device_id, as_index=False).apply(
+        lambda gdf: gdf.pipe(add_arrivals, StopFeatures.from_last_ping).pipe(
+            lambda _df: add_arrivals(
+                _df.loc[lambda df: df[Labeler.place_label] > -1, :],
+                StopFeatures.from_last_stop,
+            )
+        )
+    ).reset_index(drop=True)
+
+
+base_groupers = [
+    FilteredPingFeatures.device_id,
+    FilteredPingFeatures.year_month,
+    FilteredPingFeatures.dayofmonth,
+]
 stop_table = ScruTable(
     features=StopFeatures,
     partitioning_cols=base_groupers[1:],
@@ -110,9 +163,15 @@ def proc_partition(partition_path, model, day: DaySetup):
             if not isinstance(e, NoStops):
                 logger.exception(e)
             pass
-
-    if dfs:
-        pd.concat(dfs).pipe(_gb_stop, day.min_work_hours).pipe(stop_table.extend, verbose=False, try_dask=False)
+    if not dfs:
+        return
+    (
+        pd.concat(dfs)
+        .pipe(_gb_stop, day.min_work_hours)
+        .pipe(pipe_assigner, LocalCoords)
+        .pipe(add_speed_cols)
+        .pipe(stop_table.extend, verbose=False, try_dask=False)
+    )
 
 
 @pipereg.register(
@@ -145,7 +204,11 @@ def step(
     )
 
     parallel_map(
-        partial(proc_partition, model=model, day=dayconf), filtered_ping_table.trepo.paths, pbar=True, dist_api="mp"
+        partial(proc_partition, model=model, day=dayconf),
+        filtered_ping_table.trepo.paths,
+        pbar=True,
+        dist_api="mp",
+        raise_errors=True,
     )
 
 
@@ -175,5 +238,4 @@ def _gb_stop(labeled_df, min_work_hours):
             }
         )
         .reset_index()
-        .loc[lambda df: df[Labeler.place_label] > -1, :]
     )
