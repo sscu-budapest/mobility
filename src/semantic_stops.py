051e5056c2e26bb0a19804f4cbd5d2345956e26b
Endre Márk Borza <endremborza@gmail.com>
2022-07-03 13:05:31 +0200

    extend semantic stops

--- src/semantic_stops.py
+++ src/semantic_stops.py
@@ -1,15 +1,16 @@
+import datetime as dt
 from dataclasses import dataclass
 from functools import partial
 
 import datazimmer as dz
-import numpy as np
 import pandas as pd
-from colassigner import Col
+from colassigner import Col, get_all_cols
+from metazimmer.gpsping.ubermedia import Coordinates
 
 from .filtered_stops import filtered_stop_table
 from .initial_stops import Stop
 from .speed_based_filter import Arrival
-from .util import get_client
+from .util import localize
 
 
 @dataclass
@@ -44,10 +45,6 @@ class ArrivalFromStop(ArrivalFromPing):
 
 
 class StopExtension(dz.AbstractEntity):
-    def __init__(self, df, day: DaySetup) -> None:
-        self._morning_end = self._get_hour(df, day.morning_end)
-        self._work_end = self._get_hour(df, day.work_end)
-        self._evening_start = self._get_hour(df, day.home_arrive)
 
     from_last_stop = ArrivalFromStop
     from_last_ping = ArrivalFromPing
@@ -55,48 +52,155 @@ class StopExtension(dz.AbstractEntity):
     def duration(self, df) -> Col[float]:
         return (df[Stop.interval.end] - df[Stop.interval.start]).dt.total_seconds() / 60
 
-    def morning_rate(self, df) -> Col[float]:
-        morning_period = self._morning_end - df[Stop.interval.start]
-        return self._get_rate(df, morning_period)
 
-    def work_rate(self, df) -> Col[float]:
-        start_time = np.maximum(df[Stop.interval.start], self._morning_end)
-        end_time = np.minimum(df[Stop.interval.end], self._work_end)
-        return self._get_rate(df, end_time - start_time)
+class SpecialPlace(dz.CompositeTypeBase):
 
-    def evening_rate(self, df) -> Col[float]:
-        return self._get_rate(df, df[Stop.interval.end] - self._evening_start)
-
-    def _get_rate(self, df, spent_ext):
-        spent_within = spent_ext.dt.total_seconds() / 60
-        return np.clip(spent_within / df[StopExtension.duration], 0, 1)
-
-    def _get_hour(self, df, hour):
-        return df[Stop.interval.start].dt.normalize() + pd.Timedelta(hours=hour)
+    time = float  # minutes
+    total_time_in_period = float  # minutes
+    is_max_in_period = bool
+    distance = float  # meters
 
 
 class SemanticStop(Stop, StopExtension):
 
-    pass
+    time_bin = dt.datetime
 
-    # is_home = bool
-    # is_work = bool
+    home = SpecialPlace
+    work = SpecialPlace
 
 
-def proc_device_group_partition(gdf, dayconf: DaySetup, out_table):
+def proc_device_group_partition(gdf, dayconf: DaySetup, period, out_table):
     out_table.replace_groups(
-        gdf.groupby(Stop.device_id).apply(lambda df: StopExtension(df, dayconf)(df))
+        gdf.groupby(Stop.device_id, as_index=False).apply(proc_device, dayconf, period)
     )
 
 
-semantic_stop_table = dz.ScruTable(SemanticStop, partitioning_cols=filtered_stop_table.partitioning_cols)
+def proc_device(dedf, dayconf: DaySetup, period: str):
+
+    extended_df = (
+        _get_times_df(dedf, dayconf)
+        .merge(dedf.pipe(StopExtension()))
+        .assign(
+            **{
+                SemanticStop.time_bin: lambda df: df[Stop.interval.start]
+                .dt.to_period(period)
+                .dt.to_timestamp()
+            }
+        )
+    )
+    _gcols = [Stop.place_label, SemanticStop.time_bin]
+    _places = [SemanticStop.work, SemanticStop.home]
+    period_sums = (
+        extended_df.groupby(_gcols)[[_p.time for _p in _places]]
+        .transform("sum")
+        .rename(columns={_p.time: _p.total_time_in_period for _p in _places})
+    )
+    period_maxes = (
+        pd.concat([period_sums, extended_df[_gcols]], axis=1)
+        .loc[~extended_df[Stop.is_between_stops], :]
+        .groupby(_gcols)
+        .transform("max")
+        .rename(columns=lambda s: f"max_{s}")
+    )
+
+    return (
+        pd.concat([extended_df, period_sums, period_maxes], axis=1)
+        .fillna(0)
+        .pipe(
+            lambda df: df.assign(
+                **{
+                    _p.is_max_in_period: (
+                        df[_p.total_time_in_period]
+                        == df[f"max_{_p.total_time_in_period}"]
+                    )
+                    for _p in _places
+                }
+            )
+        )
+        .pipe(
+            lambda df: df.assign(
+                **{_p.distance: _get_dist_from_latest(df, _p) for _p in _places}
+            )
+        )
+    )
+
+
+semantic_stop_table = dz.ScruTable(
+    SemanticStop, partitioning_cols=filtered_stop_table.partitioning_cols
+)
 
 
 @dz.register(dependencies=[filtered_stop_table], outputs=[semantic_stop_table])
-def step(morning_end: int, work_end: int, home_arrive: int):
+def step(morning_end: int, work_end: int, home_arrive: int, time_unit: str):
 
     dayconf = DaySetup(morning_end, work_end, home_arrive)
     filtered_stop_table.map_partitions(
-        fun=partial(proc_device_group_partition, dayconf=dayconf, out_table=semantic_stop_table),
+        fun=partial(
+            proc_device_group_partition,
+            dayconf=dayconf,
+            period=time_unit,
+            out_table=semantic_stop_table,
+        ),
         pbar=True,
     )
+
+
+def _get_times_df(dedf: pd.DataFrame, dayconf: DaySetup) -> pd.DataFrame:
+    int_cols = [Stop.interval.start, Stop.interval.end]
+    return (
+        pd.concat([dedf.assign(_d=dedf[c]) for c in int_cols])
+        .set_index("_d")
+        .sort_index()
+        .groupby(Stop.stop_number)
+        .resample("1H")[int_cols]
+        .first()
+        .assign(
+            _s=lambda df: df[int_cols[0]].fillna(method="ffill"),
+            _e=lambda df: df[int_cols[1]].fillna(method="bfill"),
+        )
+        .reset_index()
+        .assign(
+            _d2=lambda df: df["_d"] + pd.Timedelta(hours=1),
+            dur=lambda df: (
+                df[["_e", "_d2"]].min(axis=1) - df[["_s", "_d"]].max(axis=1)
+            ).dt.total_seconds()
+            / 60,
+            weekend=lambda df: df["_d"].dt.dayofweek.isin([5, 6]),
+            hour=lambda df: df["_d"].dt.hour,
+            **{
+                SemanticStop.home.time: lambda df: (
+                    (df["hour"] <= dayconf.morning_end)
+                    | (df["hour"] >= dayconf.home_arrive)
+                )
+                * df["dur"],
+                SemanticStop.work.time: lambda df: (
+                    (df["hour"] > dayconf.morning_end)
+                    & (df["hour"] <= dayconf.work_end)
+                    & ~df["weekend"]
+                )
+                * df["dur"],
+            },
+        )
+        .groupby(Stop.stop_number)[[SemanticStop.home.time, SemanticStop.work.time]]
+        .sum()
+        .reset_index()
+    )
+
+
+def _loc(df, coords: Coordinates, filter_col):
+    return pd.DataFrame(
+        {
+            col: df[col]
+            .where(df[filter_col])
+            .fillna(method="ffill")
+            .fillna(method="bfill")
+            for col in get_all_cols(coords)
+        },
+        index=df.index,
+    )
+
+
+def _get_dist_from_latest(df, place: SpecialPlace):
+    start_points = localize(df, Stop.center)
+    end_points = localize(_loc(df, Stop.center, place.is_max_in_period), Stop.center)
+    return start_points.distance(end_points)
