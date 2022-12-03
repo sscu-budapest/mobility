c66fa2c95c817343425b9f5d3be7959585dc5b33
Endre Márk Borza <endremborza@gmail.com>
2022-12-03 17:17:13 +0100

    run h3 agg

--- src/h3_hour_aggregate.py
+++ src/h3_hour_aggregate.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from functools import partial
+from functools import partial, reduce
 from multiprocessing import cpu_count
 
 import datazimmer as dz
@@ -27,8 +27,16 @@ def get_h3_id(df):
     return df.pipe(to_geo).h3.geo_to_h3(10).index.to_numpy()
 
 
-def proc_month_groups(paths, min_count, min_duration):
-    gdf = pd.concat(map(pd.read_parquet, paths))
+def proc_month_groups(paths_tup, min_count, min_duration):
+    paths = paths_tup[1]
+    fun = partial(proc_partition_path, min_count=min_count, min_duration=min_duration)
+    red_df = reduce(_reducer, map(fun, paths))
+    hour_col = red_df.index.get_level_values(HashHour.hour).astype(str).str[:7]
+    h3_table.extend(red_df.assign(**{HashHour.year_month: hour_col}))
+
+
+def proc_partition_path(path, min_count, min_duration):
+    gdf = pd.read_parquet(path)
     agg_df = (
         gdf.assign(
             **{
@@ -43,8 +51,8 @@ def proc_month_groups(paths, min_count, min_duration):
         )
     )
     if agg_df.empty:
-        return
-    h3_df = (
+        return pd.DataFrame(index=pd.MultiIndex(names=h3_table.index_cols))
+    return (
         agg_df.loc[
             lambda df: (df["count"] >= min_count)
             & (df["duration_minutes"] >= min_duration)
@@ -52,8 +60,6 @@ def proc_month_groups(paths, min_count, min_duration):
         .groupby(h3_table.index_cols)[[HashHour.count]]
         .agg("count")
     )
-    hour_col = h3_df.index.get_level_values(HashHour.hour).astype(str).str[:7]
-    h3_table.extend(h3_df.assign(**{HashHour.year_month: hour_col}))
 
 
 @dz.register(dependencies=[ping_table], outputs=[h3_table])
@@ -64,10 +70,18 @@ def step(min_count, min_duration):
     proc_paths = partial(
         proc_month_groups, min_count=min_count, min_duration=min_duration
     )
-    iterable = [
-        list(ps)
-        for _, ps in ping_table.get_partition_paths(
-            partition_col=meta.ExtendedPing.year_month
-        )
-    ]
+    iterable = ping_table.get_partition_paths(
+        partition_col=meta.ExtendedPing.year_month
+    )
     list(parallel_map(proc_paths, iterable, pbar=True, workers=workers))
+
+
+def _reducer(df1: pd.DataFrame, df2: pd.DataFrame):
+    name = df1.columns[0]
+    return (
+        df1.join(df2, how="outer", rsuffix="r")
+        .fillna(0)
+        .sum(axis=1)
+        .rename(name)
+        .to_frame()
+    )
